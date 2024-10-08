3438d794707c42e63bf64cb68e242f1825dd4558
Andrey Endovitskiy <aendov56@gmail.com>
2024-10-08 12:56:51 +0700

    reanswer to task 3 module 8

--- MODULE_8/module_8_3.py
+++ MODULE_8/module_8_3.py
@@ -7,20 +7,20 @@ class Car():
         self.__is_valid_vin(self.__vin)
         self.__is_valid_numbers(self.__numbers)
 
-        def __is_valid_vin(vin_number):
-            if not isinstance(vin_number, int):
-                raise IncorrectVinNumber('Некорректный тип vin номер')
-            elif vin_number < 1000000 or vin_number > 9999999:
-                raise IncorrectVinNumber('Неверный диапазон для vin номера')
-            else:
-                return True
-        def __is_valid_numbers(numbers):
-            if not isinstance(numbers, str):
-                raise IncorrectCarNumbers('Некорректный тип данных для номеров')
-            elif len(numbers) != 6:
-                raise IncorrectCarNumbers('Неверная длина номера')
-            else:
-                return True
+    def __is_valid_vin(self,vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+        elif vin_number < 1000000 or vin_number > 9999999:
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        else:
+            return True
+    def __is_valid_numbers(self, numbers):
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        elif len(numbers) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
+        else:
+            return True
 
 
 class IncorrectVinNumber(Exception):

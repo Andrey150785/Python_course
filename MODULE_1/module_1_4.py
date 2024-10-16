5dd2973f03e16583fdaebe6ba690176d1bcaddbf
Andrey Endovitskiy <aendov56@gmail.com>
2024-10-16 21:44:50 +0700

    reanswer to task 4 module 1

---
+++ MODULE_1/module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('введите строку: ')
+print(len(my_string))
+print(my_string.lower())
+print(my_string.upper())
+print(''.join(my_string.split()))
+print(my_string[0])
+print(my_string[-1])

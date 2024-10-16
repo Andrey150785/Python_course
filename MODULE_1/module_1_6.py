e81b58508fbba22558c8f1633b087da27df8e29c
Andrey Endovitskiy <aendov56@gmail.com>
2024-10-16 22:01:28 +0700

    reanswer to task 6 module 1

---
+++ MODULE_1/module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Anrey': 1985, 'Boris': 2000, 'Chris': 1993}
+print(my_dict)
+print(my_dict['Anrey'])
+my_dict['Denis'] = 1990
+my_dict['Vera'] = 1995
+del my_dict['Anrey']
+print(my_dict)
+
+my_set = {1, 2, True, 3, True, 5, 1, 2}
+print(my_set)
+my_set.add(10)
+my_set.remove(3)
+print(my_set)

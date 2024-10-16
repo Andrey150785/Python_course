3bb7c2166e74cfc3c281b439b847bc99970320cf
Andrey Endovitskiy <aendov56@gmail.com>
2024-10-16 21:25:02 +0700

    reanswer to task practice module 1

---
+++ MODULE_1/Practice.py
@@ -0,0 +1,6 @@
+task_count = 12
+all_time = 1.5
+task_name = 'Python'
+task_time = all_time / task_count
+
+print('Курс: ', task_name, ', всего задач:', task_count, ', затрачено часов:', all_time, ', среднее время выполнения ', task_time, ' часа.', sep = '')

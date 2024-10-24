44fa5e9177ffc8a611419967ff04428a826e9bb3
Andrey Endovitskiy <aendov56@gmail.com>
2024-10-24 22:31:42 +0700

    answer to task 2 module 11

---
+++ MODULE_11/Inspection.py
@@ -0,0 +1,24 @@
+﻿import sys
+import inspect
+
+def introspection_info(obj):
+    dict_ = {}
+    dict_['type'] = type(obj) # - Тип объекта
+    attributes = []
+    methods = []
+    for attr_name in dir(obj):
+        attr_value = getattr(obj, attr_name)
+        if callable(attr_value):
+            methods.append(attr_name)  # Если атрибут является вызываемым (методом), добавляем в список методов
+        else:
+            attributes.append(attr_name)  # В противном случае добавляем в список атрибутов
+    dict_['attributes'] = attributes # - Атрибуты объекта
+    dict_['methods'] = methods # - Методы объекта
+    dict_['module'] = inspect.getmodule(obj) # - Модуль, к которому объект принадлежит
+    dict_['size'] = sys.getsizeof(obj)
+
+    return dict_
+
+
+number_info = introspection_info(42)
+print(number_info)
